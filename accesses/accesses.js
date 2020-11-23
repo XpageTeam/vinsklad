e428bbffcb0857e0da570297d05171fd136b9966
Danil Khandys <dreadlord74@yandex.ru>
2020-11-23 13:42:36 +0500

    добавил доступы

---
+++ accesses/accesses.js
@@ -0,0 +1,14 @@
+module.exports = {
+    xpager: {
+        dirName: "/www/html.xpager.ru/vinsklad/",
+        host: 'html.xpager.ru',
+        user: 'file',
+        password: 'X9y7E5d0'
+    },
+    server: {
+        path: "/htdocs/local/templates/vinsklad2017/",
+        host: "95.213.253.51",
+        user: "vin.xpager.ru",
+        password: "sZayIb6Q",
+    }
+}
